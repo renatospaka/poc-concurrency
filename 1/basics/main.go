e3dad998e5c96e1366cb5f443669e96b7e2478c1
Renato Spakauskas <renatospaka@gmail.com>
2024-04-01 10:45:57 -0300

    feat: :sparkles: básico de select em channels

---
+++ 1/basics/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 1)
+	ch <- 10
+	select {
+	case val := <- ch:
+		fmt.Printf("Recebido 1 de ch: %d\n", val)
+	case val := <- ch:
+		fmt.Printf("Recebido 2 de ch: %d\n", val)
+	case val := <- ch:
+		fmt.Printf("Recebido 3 de ch: %d\n", val)
+	default:
+		fmt.Println("esse é o default")
+	}
+}
